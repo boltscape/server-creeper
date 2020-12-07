2a1ba0d2766569e642a3ec7e419db73615a908fb
kaiser2343 <kaiser.shaik@gmail.com>
2020-12-07 13:59:19 +0530

    Add code comments

--- main.py
+++ main.py
@@ -3,21 +3,28 @@ from googleapiclient import discovery
 from google.oauth2 import service_account
 
 from discord import Game
-import requests
 import os
 from time import sleep
 
-TOKEN = OS.ENVIRON['BOT_TOKEN']
+bot_token = '' #Enter your Discord bot token here
+serveraddress = '' #Enter your server (Google Compute instance) IP address here
+
+#Setting the command prefix for the bot and creating a bot object
 BOT_PREFIX = ('!')
 client = Bot(command_prefix=BOT_PREFIX)
 
+#Generating credentials object from JSON file
 credentials = service_account.Credentials.from_service_account_file('creds.json')
 
+#Building an object for the Compute instance
 service = discovery.build('compute', 'v1', credentials=credentials)
-project = 'feisty-ceiling-285406' 
-zone = 'asia-southeast1-b'  
-instance = 'mc-server'
+project = '' #Your Google Cloud project ID
+zone = ''  #The zone that your VM is in
+instance = '' #Your VM's name
 
+#Function definitions start here
+
+#The 5 core functions: start, stop, restart, status, and address. Pretty self-explainatory.
 @client.command(name="start", description="Starts the Minecraft server", brief="Starts server", pass_context=True)
 async def startserver(ctx):
     request = service.instances().start(project=project, zone=zone, instance=instance)
@@ -55,6 +62,11 @@ async def serverstatus(ctx):
     elif response['status'] == 'TERMINATED':
         await ctx.send("Ssserver is off, " + ctx.author.mention)
 
+#Some whimsical commands I wrote for my friends
+@client.command(description="Show the server address", brief="Show server address", pass_context=True)
+async def address(ctx):
+    await ctx.send("Hey "+ ctx.author.mention + ", use " + serveraddress + " to connect to the server!")
+
 @client.command(name="hi", description="Say hi!", brief="Hi!", pass_context=True)
 async def thanks(ctx):
     await ctx.send("Hi, " + ctx.author.mention + " :smile:")
@@ -63,13 +75,10 @@ async def thanks(ctx):
 async def thanks(ctx):
     await ctx.send("You're welcome, " + ctx.author.mention + " :smile:")
     
-@client.command(description="Show the server address", brief="Show server address", pass_context=True)
-async def address(ctx):
-    await ctx.send("Hey "+ ctx.author.mention + ", use " + os.environ['SERVER_ADDRESS'] + " to connect to the server!")
-
+#Set the bot's activity in Discord to playing Minecraft
 @client.event
 async def on_ready():
     await client.change_presence(activity=Game(name="Minecraft"))
 
-                                 
-client.run(TOKEN)
+#Run the bot
+client.run(bot_token)
